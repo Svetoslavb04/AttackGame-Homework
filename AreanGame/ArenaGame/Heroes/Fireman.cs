84ce2f3116aa60d74de48c5840feb1490ac42dea
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-31 21:43:08 +0300

    Fireman hero added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ AreanGame/ArenaGame/Heroes/Fireman.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Fireman: Hero
+    {
+        private int firesMade;
+        public Fireman(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+            firesMade = 0;
+        }
+
+        public override double Attack()
+        {
+            double number = random.NextDouble();
+
+            if (number < 0.15)
+            {
+                firesMade++;
+            }
+
+            return base.Attack();
+        }
+
+        public override double Defend(double damage)
+        {
+            if (firesMade % 7 == 0)
+            {
+                damage = 0;
+            }
+            
+            return base.Defend(damage);
+        }
+    }
+}
