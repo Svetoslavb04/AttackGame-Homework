3b559cbbb85f0f87b75098cf30e2095404ffc8c0
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-31 21:59:32 +0300

    Bow added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ AreanGame/ArenaGame/Weapons/Bow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Weapons
+{
+    public class Bow : IWeapon
+    {
+        private double attackDamage;
+
+        public int Arrows { get; set; }
+
+        public string Name { get; set; }
+
+        public double AttackDamage 
+        { 
+            get 
+            {
+                if (Arrows > 0)
+                {
+                    Arrows--;
+                    return attackDamage;
+                }
+                return 0;
+            }
+            private set 
+            {
+                attackDamage = value;
+            } 
+        }
+
+        public double BlockingPower { get; private set; }
+
+
+        public Bow(string name)
+        {
+            Name = name;
+            AttackDamage = 20;
+            BlockingPower = 10;
+        }
+        
+    }
+}
