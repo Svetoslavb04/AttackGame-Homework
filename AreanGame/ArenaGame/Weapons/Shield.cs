c82563d4db204616e31d821c322d8fa598ce0984
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-31 21:59:41 +0300

    Shield added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ AreanGame/ArenaGame/Weapons/Shield.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Weapons
+{
+    public class Shield : IWeapon
+    {
+        private double blockingPower;
+
+        public int Arrows { get; set; }
+
+        public string Name { get; set; }
+
+        public double AttackDamage { get; private set; }
+
+        public double BlockingPower 
+        { 
+            get 
+            {
+                blockingPower *= 0.95;
+                return blockingPower;
+            }
+            private set 
+            {
+                blockingPower = value;
+            } 
+        }
+
+        public Shield(string name)
+        {
+            Name = name;
+            AttackDamage = 2;
+            BlockingPower = 100;
+        }
+        
+        public void Strengthen(double blockingPower) {
+            BlockingPower += blockingPower;
+        }
+    }
+}
