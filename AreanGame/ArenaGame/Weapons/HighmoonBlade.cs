0f36e6aeeaa3909352afc466de0951fb399e74a4
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-31 21:59:39 +0300

    HighmoonBlade added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ AreanGame/ArenaGame/Weapons/HighmoonBlade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Weapons
+{
+    public class HighmoonBlade : IWeapon
+    {
+        public string Name { get; set; }
+
+        public double AttackDamage { get; private set; }
+
+        public double BlockingPower { get; private set; }
+
+        public HighmoonBlade(string name)
+        {
+            Name = name;
+            AttackDamage = 20;
+            BlockingPower = 10;
+        }
+
+        public void ActivateHighMoon() {
+            AttackDamage *= 2;
+            BlockingPower *= 1.2;
+        }
+    }
+}
