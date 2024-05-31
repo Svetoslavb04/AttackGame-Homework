20678b7a0741983fceeb66ecb282eae25cf1b52d
Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>
2024-05-31 21:43:19 +0300

    Villian hero added
    Signed-off-by: Svetoslav Borislavov <svetoslav.borislavov@limechain.tech>

---
+++ AreanGame/ArenaGame/Heroes/Villian.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Villian: Hero
+    {
+        public Villian(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+        }
+
+        public override double Attack()
+        {
+            return 0;
+        }
+
+        public override double Defend(double damage)
+        {
+            return base.Defend(Health);
+        }
+    }
+}
